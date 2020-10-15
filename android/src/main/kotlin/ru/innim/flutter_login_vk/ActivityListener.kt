4619c75a15dff0de5bded9df328b60884d21c5e9
e.safronov <e.safronov@free-lance.ru>
2020-10-15 11:27:23 +0300

    Fix nullable data parameter

--- android/src/main/kotlin/ru/innim/flutter_login_vk/ActivityListener.kt
+++ android/src/main/kotlin/ru/innim/flutter_login_vk/ActivityListener.kt
@@ -5,8 +5,7 @@ import com.vk.api.sdk.VK.onActivityResult
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
 
 class ActivityListener(private val _loginCallback: LoginCallback) : ActivityResultListener {
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Boolean {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
         return onActivityResult(requestCode, resultCode, data, _loginCallback)
     }
-
 }
